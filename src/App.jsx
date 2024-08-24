935b51d361b5877633d1ab2a6bc2f5ab5443fef9
sahilsharma3942 <sahilsharma3942@gmail.com>
2024-08-24 19:21:19 +0530

    react-router-dom module res

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import Product from "./pages/Product/Product";
 import "./app.scss";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 function Layout() {
   return (
